a774fa991f8ad7522640bc38ca0cf8ad3a38bf19
Rajat <rajwahi97@gmail.com>
2023-11-14 19:40:03 +0530

    updated

---
+++ loginSignUp/App.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigation from './navigation/AppNavigation'
+
+const App = () => {
+  return (
+ <View>
+
+  <Text>
+
+    hello
+  </Text>
+ </View>
+  )
+}
+
+export default App
